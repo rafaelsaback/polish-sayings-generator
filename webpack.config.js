6344a9e48e9fe2396550180f42707832636a8b2b
Rafael Saback <rafaelsaback@gmail.com>
2018-10-29 18:38:09 +0100

    Indent webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,32 @@
 path = require("path");
 
 module.exports = {
-  mode: 'development',
-  entry: ['./src/scripts/script.js', './src/styles/style.scss'],
-  output: {
-    filename: 'bundle.js',
-    path: path.join(__dirname, 'dist')
-  },
-  devtool: "source-map",
-  devServer: {
-      inline: true,
-      contentBase: path.join(__dirname, 'dist'),
-      port: 3000,
-      open: true
-  },
-  resolve: {
-      extensions: [".tsx", ".ts", ".js"],
-      modules: [
-          path.resolve(__dirname, 'src'),
-          'node_modules'
-      ]
-  },
-  module: {
-    rules: [
-      {
-        test:/\.(s*)css$/,
-        use:['style-loader','css-loader', 'sass-loader']
-      }
-    ]
-  },
+    mode: 'development',
+    entry: ['./src/scripts/script.js', './src/styles/style.scss'],
+    output: {
+        filename: 'bundle.js',
+        path: path.join(__dirname, 'dist')
+    },
+    devtool: "source-map",
+    devServer: {
+        inline: true,
+        contentBase: path.join(__dirname, 'dist'),
+        port: 3000,
+        open: true
+    },
+    resolve: {
+        extensions: [".tsx", ".ts", ".js"],
+        modules: [
+            path.resolve(__dirname, 'src'),
+            'node_modules'
+        ]
+    },
+    module: {
+        rules: [
+            {
+                test:/\.(s*)css$/,
+                use:['style-loader','css-loader', 'sass-loader']
+            }
+        ]
+    },
 };
