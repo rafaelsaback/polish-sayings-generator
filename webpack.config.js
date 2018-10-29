6fd63c1aceeea9cac652b6a62cc95c7d8fba94ff
Rafael Saback <rafaelsaback@gmail.com>
2018-10-29 18:27:51 +0100

    Remove typescript (unnecessary)

--- webpack.config.js
+++ webpack.config.js
@@ -2,19 +2,14 @@ path = require("path");
 
 module.exports = {
   mode: 'development',
-  entry: ['./src/scripts/script.ts', './src/styles/style.scss'],
+  entry: ['./src/scripts/script.js', './src/styles/style.scss'],
   output: {
-    filename: 'script.js',
+    filename: 'bundle.js',
     path: path.join(__dirname, 'dist')
   },
   devtool: "source-map",
   module: {
     rules: [
-      {
-        test: /\.tsx?$/,
-        loader: 'ts-loader',
-        exclude: /node_modules/,
-      },
       {
         test:/\.(s*)css$/,
         use:['style-loader','css-loader', 'sass-loader']
