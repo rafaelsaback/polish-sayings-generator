647c6f9976b3d6fe748b560a6259f436b348a103
Rafael Saback <rafaelsaback@gmail.com>
2018-10-29 21:39:52 +0100

    Refactoring

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import sayings from 'sayings.json';
+import 'style.css';
+
+const polishSaying = document.querySelector('#polish-saying');
+const equivalentSaying = document.querySelector('#equivalent-saying');
+const translatedSaying = document.querySelector('#translated-saying');
+const btn = document.querySelector('#new-saying-button');
+
+const generateNewSaying = function() {
+    let range = sayingsList.length;
+    let randomNum = Math.floor(Math.random() * range);
+    let saying = sayingsList[randomNum];
+    polishSaying.innerHTML = `"${saying['saying']}"`;
+    equivalentSaying.innerHTML = `"${saying['equivalent-en']}"`;
+    translatedSaying.innerHTML = `"${saying['translation-en']}"`;
+    sayingsList.splice(randomNum, 1);
+    if(sayingsList.length === 0){
+        sayingsList = sayings.slice();
+    }
+};
+
+var sayingsList = sayings.slice();
+generateNewSaying();
+btn.addEventListener('click', generateNewSaying);
